0b221b9c66201efd0b4d64ec1904331f0e2da3be
Artem Krop <qazy777@gmail.com>
2023-05-11 23:54:30 +0300

    upd(all): changed mods, added comments

--- deb_init.sh
+++ deb_init.sh
@@ -2,10 +2,10 @@
 
 docker-compose up -d;
 
-chmod -R 777 pgadmin_data;
+chmod -R 770 pgadmin_data;  # allow group write and disallow anything by others
 echo 'chmod pgadmin_data success';
 
-function check_postgres_is_ready() {
+function check_postgres_is_ready() {  # checks if postgres is completely up
   if docker-compose logs postgres | grep 'database system is ready to accept connections'; then
     echo "postgres is ready";
     return "1";
@@ -15,14 +15,13 @@ function check_postgres_is_ready() {
   fi
 }
 
-while check_postgres_is_ready
+while check_postgres_is_ready  # time delay for postgres deployment
 do
-  docker-compose logs postgres | grep 'database system is ready to accept connections';
-  echo 'wait';
+  echo 'wait for postgres';
   sleep 1;
 done
 
-chmod -R 777 pgdata;
+chmod -R 770 pgdata; # allow rwx for group
 echo 'chmod pgdata success'
 
 docker-compose logs -f;
